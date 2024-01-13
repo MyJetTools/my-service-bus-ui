216ae77a502467bbe8183edaae98d0b06cf44b54
amigin <amigin@gmail.com>
2024-01-14 00:21:54 +0200

    Fixed mem formatting

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,7 @@
 pub fn format_mem(mem: i64) -> String {
     let mem = mem as f64;
     if mem < 1024.0 {
-        return format!("{:.3}B", mem);
+        return format!("{}B", mem);
     }
 
     let mem = mem / 1024.0;
