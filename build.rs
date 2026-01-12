0c28880abf7dd87084b3e452f6f4e55fc075a083
amigin <amigin@gmail.com>
2026-01-12 17:27:01 +0200

    ci-fix

--- build.rs
+++ build.rs
@@ -4,5 +4,6 @@ fn main() {
     CiGenerator::new(env!("CARGO_PKG_NAME"))
         .as_dioxus_fullstack_service()
         .generate_github_ci_file()
+        .set_docker_image_name("ghcr.io/myjettools/my-service-bus-ui")
         .build();
 }
